88d7060a743308c16a1002e940824bb9ec91ea2c
ColmMeliodas <36660747+ColmErdenetogs@users.noreply.github.com>
2018-03-08 11:16:28 +0000

    Add files via upload

--- fishmoley.sql
+++ fishmoley.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2018 at 09:24 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Generation Time: Mar 08, 2018 at 12:15 PM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,12 +30,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `addresses` (
   `address_id` int(10) NOT NULL,
-  `country` int(10) NOT NULL,
+  `country` varchar(20) NOT NULL,
   `line_1` varchar(50) NOT NULL,
   `line_2` varchar(50) DEFAULT NULL,
   `city` varchar(50) NOT NULL,
-  `state_province_county` varchar(50) NOT NULL,
-  `zip_code` varchar(20) NOT NULL
+  `state_province_county` varchar(50) DEFAULT NULL,
+  `zip_code` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -56,8 +58,7 @@ CREATE TABLE `category` (
 CREATE TABLE `products` (
   `product_id` int(10) NOT NULL,
   `category_id` int(10) NOT NULL,
-  `supplier_id` int(10) NOT NULL,
-  `product_type_code` int(10) NOT NULL
+  `supplier_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -83,19 +84,8 @@ CREATE TABLE `users` (
   `user_password` varchar(300) NOT NULL,
   `user_email` varchar(50) NOT NULL,
   `user_phone` varchar(50) DEFAULT NULL,
-  `admin_status` int(10) NOT NULL DEFAULT '0'
+  `admin_status` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_email`, `user_phone`, `admin_status`) VALUES
-(1, 'tomas', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'tomas@yahoo.ie', '123456789', 1),
-(2, 'colm', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'colm@gmail.com', '123456', 0),
-(3, 'Nifemi', 'b3a8e0e1f9ab1bfe3a36f231f676f78bb3a519d2b21e6c530c0eee8ebb4a5d0', 'Nifemi@gmail.com', '123456789', 0),
-(4, 'Darcy', '83cf8b609de6036a8277bd0e96135751bbc7eb234256d4b65b893360651bf2', 'Darcy@email.com', '678', 0),
-(5, 'NotTomas', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'nottomas@yahoo.ie', '123', 0);
 
 -- --------------------------------------------------------
 
@@ -106,7 +96,7 @@ INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_email`, `user
 CREATE TABLE `user_addresses` (
   `user_id` int(10) NOT NULL,
   `address_id` int(10) NOT NULL,
-  `date_from` date DEFAULT NULL
+  `date_from` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -119,7 +109,7 @@ CREATE TABLE `user_orders` (
   `order_id` int(10) NOT NULL,
   `user_id` int(10) NOT NULL,
   `order_status_code` int(10) NOT NULL,
-  `date_order_placed` date DEFAULT NULL,
+  `date_order_placed` date NOT NULL,
   `order_price` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -183,7 +173,7 @@ ALTER TABLE `user_addresses`
 --
 ALTER TABLE `user_orders`
   ADD PRIMARY KEY (`order_id`),
-  ADD KEY `fk_user2_id` (`user_id`);
+  ADD KEY `fk_user_id_2` (`user_id`);
 
 --
 -- Indexes for table `user_order_products`
@@ -192,15 +182,6 @@ ALTER TABLE `user_order_products`
   ADD KEY `fk_order_id` (`order_id`),
   ADD KEY `fk_product_id` (`product_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- Constraints for dumped tables
 --
@@ -223,7 +204,7 @@ ALTER TABLE `user_addresses`
 -- Constraints for table `user_orders`
 --
 ALTER TABLE `user_orders`
-  ADD CONSTRAINT `fk_user2_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+  ADD CONSTRAINT `fk_user_id_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 
 --
 -- Constraints for table `user_order_products`
@@ -231,6 +212,7 @@ ALTER TABLE `user_orders`
 ALTER TABLE `user_order_products`
   ADD CONSTRAINT `fk_order_id` FOREIGN KEY (`order_id`) REFERENCES `user_orders` (`order_id`),
   ADD CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
