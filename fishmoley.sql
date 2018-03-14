5c917d3e9dde524a2e9852c788de31ea530389fb
ColmMeliodas <36660747+ColmErdenetogs@users.noreply.github.com>
2018-03-14 15:20:29 +0000

    Add files via upload

--- fishmoley.sql
+++ fishmoley.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2018 at 02:25 PM
+-- Generation Time: Mar 14, 2018 at 04:19 PM
 -- Server version: 10.1.24-MariaDB
 -- PHP Version: 7.1.6
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fishmoleyexp`
+-- Database: `fishmoley`
 --
 
 -- --------------------------------------------------------
@@ -69,6 +69,8 @@ CREATE TABLE `users` (
   `user_id` int(10) NOT NULL,
   `username` varchar(20) NOT NULL,
   `user_password` varchar(300) NOT NULL,
+  `address_line1` varchar(200) NOT NULL,
+  `address_line2` varchar(200) NOT NULL,
   `user_email` varchar(50) NOT NULL,
   `user_phone` varchar(50) DEFAULT NULL,
   `admin_status` int(10) NOT NULL DEFAULT '0'
@@ -78,12 +80,12 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `username`, `user_password`, `user_email`, `user_phone`, `admin_status`) VALUES
-(1, 'tomas', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'tomas@yahoo.ie', '123456789', 1),
-(2, 'colm', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'colm@gmail.com', '123456', 0),
-(3, 'Nifemi', 'b3a8e0e1f9ab1bfe3a36f231f676f78bb3a519d2b21e6c530c0eee8ebb4a5d0', 'Nifemi@gmail.com', '123456789', 0),
-(4, 'Darcy', '83cf8b609de6036a8277bd0e96135751bbc7eb234256d4b65b893360651bf2', 'Darcy@email.com', '678', 0),
-(5, 'NotTomas', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 'nottomas@yahoo.ie', '123', 0);
+INSERT INTO `users` (`user_id`, `username`, `user_password`, `address_line1`, `address_line2`, `user_email`, `user_phone`, `admin_status`) VALUES
+(1, 'tomas', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '', '', 'tomas@yahoo.ie', '123456789', 1),
+(2, 'colm', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '', '', 'colm@gmail.com', '123456', 0),
+(3, 'Nifemi', 'b3a8e0e1f9ab1bfe3a36f231f676f78bb3a519d2b21e6c530c0eee8ebb4a5d0', '', '', 'Nifemi@gmail.com', '123456789', 0),
+(4, 'Darcy', '83cf8b609de6036a8277bd0e96135751bbc7eb234256d4b65b893360651bf2', '', '', 'Darcy@email.com', '678', 0),
+(5, 'NotTomas', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '', '', 'nottomas@yahoo.ie', '123', 0);
 
 -- --------------------------------------------------------
 
